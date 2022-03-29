6940c70d67294321a343a52646a3f65378e1db4d
Stopalov12 <stopalov111@gmail.com>
2022-03-30 01:29:42 +0300

    Krypt-finished

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
+import { TransactionContext } from "../context/TransactionContext";
 import { Loader } from "./";
-
+import { shortenAddress } from "../utils/shortenAddress";
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input=({placeholder, name, type, value,handleChange})=>(
-  <input placeholder={placeholder} type={type} step='0.0001'value={value} onChange={(e)=>handleChange(e,name)} className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'/>)
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
 
+const Welcome = () => {
+  const {
+    connectWallet,
+    currentAccount,
+    formData,
+    sendTransaction,
+    handleChange,
+    isLoading
+  } = useContext(TransactionContext);
 
-
-  const Welcome = () => {
-  const connectWallet = () => {};
-
-  const handleSubmit=()=> {
-
-  }
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+    e.preventDefault();
+    if (!addressTo || !amount || !keyword || !message) return;
+    sendTransaction();
+  };
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -33,13 +49,18 @@ const Input=({placeholder, name, type, value,handleChange})=>(
             Krypto.
           </p>
           <p></p>
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#h2546bd]"
-          >
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]"
+            >
+              <AiFillPlayCircle className="text-white mr-2" />
+              <p className="text-white text-base font-semibold">
+                Connect Wallet
+              </p>
+            </button>
+          )}
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${commonStyles}`}>Reliability</div>
             <div className={commonStyles}>Security</div>
@@ -59,9 +80,7 @@ const Input=({placeholder, name, type, value,handleChange})=>(
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
               <div>
-                <p className="text-white font-light text-sm">
-                  Address
-                </p>
+                <p className="text-white font-light text-sm ">{shortenAddress(currentAccount)}</p>
                 <p className="text-white font-semibold  text-lg mt-1">
                   Ethereum
                 </p>
@@ -69,12 +88,42 @@ const Input=({placeholder, name, type, value,handleChange})=>(
             </div>
           </div>
           <div className="p-5 sm:w-96 w-ful flex flex-col justify-start items-center blue-glassmorphism">
-            <Input placeholder='Address To' name='addressTo' type='text' handleChange={()=>{}}/>
-            <Input placeholder='Amount (ETH)' name='amount' type='number' handleChange={()=>{}}/>
-            <Input placeholder='Keyword (GIF)' name='keyword' type='text' handleChange={()=>{}}/>
-            <Input placeholder='Enter Message' name='message' type='text' handleChange={()=>{}}/>
-          <div className="h-[1px] w-full bg-gray-400 my-2"/>
-         {false ? (<Loader/>) :(<button type="button" onClick={handleSubmit} className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer'>Send Now</button>)}
+            <Input
+              placeholder="Address To"
+              name="addressTo"
+              type="text"
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Amount (ETH)"
+              name="amount"
+              type="number"
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Keyword (GIF)"
+              name="keyword"
+              type="text"
+              handleChange={handleChange}
+            />
+            <Input
+              placeholder="Enter Message"
+              name="message"
+              type="text"
+              handleChange={handleChange}
+            />
+            <div className="h-[1px] w-full bg-gray-400 my-2" />
+            {isLoading ? (
+              <Loader />
+            ) : (
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+              >
+                Send Now
+              </button>
+            )}
           </div>
         </div>
       </div>
---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI=abi.abi;
+export const contractAddress= '0x7Efb4bA4e2722DC9Da593b0761B30527037F64EF';
